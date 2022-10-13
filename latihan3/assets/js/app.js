d5828254cbb9a85d29426e563eb8063c8f7cb64c
IdaDanuartha <danuart21@gmail.com>
2022-10-13 17:02:46 +0800

    latihan 3 (h-1 lomba)

---
+++ latihan3/assets/js/app.js
@@ -0,0 +1,33 @@
+const navbar = document.querySelector('nav.navbar')
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 30) {
+        navbar.classList.add('scroll')
+    } else {
+        navbar.classList.remove('scroll')
+    }
+})
+
+// Swiper script
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 60,
+    autoplay: {
+        delay: 4000,
+        // disableOnInteraction: true,
+    },
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+})
